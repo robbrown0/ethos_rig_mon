e262596dd629b6db02fbb2b95501d072e8918ae6
Rob Brown <35670752+robbrown0@users.noreply.github.com>
2018-02-06 12:52:04 -0500

    Update ethos_rig_mon.py

--- ethos_rig_mon.py
+++ ethos_rig_mon.py
@@ -14,21 +14,20 @@ import socket
 
 # set a few variables
 hashalert = 10  # Set the hashalert variable to the rate in MH/s that you want the flag the GPU as not hashing, and in trun rebooted the rig.  10 MH/s is the default.
-hashavgalert = 95 # Set the hashaverage variable to the rate in MH/s that you want the rig to reboot at.  Depends on the rigs total MH/s.
+hashavgalert = 65 # Set the hashaverage variable to the rate in MH/s that you want the rig to reboot at.  Depends on the rigs total MH/s.
 hashalert = format(float(hashalert), '.2f')
 hash = None
 hashdec = ''
 miner_id = ''
 varCardList = ''
 x = " "
+varNotifyCnt = 0
 statusmessage = "HASHING AS EXPECTED"
-errorstatusmessage = "NOT HASHING"
+errorstatusmessage = "LOW HASHRATE DETECTED"
 
 """
 # 
-# If you find this script beneficial, feel free to donate:  ETH 0xE8cBA305dBAAD26d6C505BC47Ee4fA96616bD52C, BTC 1HyVDcuXtArYyp438P5iUJ6VmWLrKLFqkc, BCH 1MuP5N1anJDmNHFBiurr2QZxPyeKAQY3wR, LTC LQ3PQCECy3sDrHXVdPuC2ybF5g5mXHC6ed
-#
-# *** NOTE *** You MUST edit lines 55, 56 (these are your CRONTAB entries) and lines 64 and 72!!  You do NOT use the < and > around the tokens.
+# If you find this script beneficial, feel free to donate:  ETH 0xE8cBA305dBAAD26d6C505BC47Ee4fA96616bD52C
 #
 # The idea of this script was adapted from a published version by Jayd Lawrence at https://gist.github.com/jaydlawrence/5a70cbf868f90bbc4cd7b0913810f310.  While I still use the same implementation of the
 # pushover service, I'm not determining hash using the udpate command, I'm parsing the miner log output, and I'm looking at each card as opposed to only the average hash rate.  I've added more easy to read
@@ -38,7 +37,7 @@ errorstatusmessage = "NOT HASHING"
 #
 # The entries at the bottom of this comment block go into the crontab for the ethos user.
 #
-# The script uses the Pushover notification service to notify you of reboots or when a card or the rig are not hashing as expected.
+# The script usese the Pushover notfication service to notify you of reboots or when a card or the rig are not hashing as expected.
 # Replace <app_token> with your app token, eg. curl -s -F "token=as9dv8jaw4e9fasdjgoa2w3t98jawl4" ... in the two lines below that go into the ethos user's crontab.
 # replace <user_token> with your user token eg. ... -F "user=rtsfgjsrtjsrtj457ujtfgtwsvka" ...
 #
@@ -52,8 +51,8 @@ errorstatusmessage = "NOT HASHING"
 #@reboot curl -s -F "token=aardkds1zdjxxxt8rn6qqfc1816s6k" -F "user=uqvkvnrbxxxhtt4iap35bzs3n3jy1y " -F "title=RIG - "$(hostname)" - IS NOW ONLINE" -F "message=RIG - "$(hostname)" rebooted and is now online." https://api.pushover.net/1/messages.json
 #*/2 * * * *   /usr/bin/python /home/ethos/ethos_rig_mon.py -u uqvkvnrb3mshtxxxap35bzs3n3jy1y -a aardkds1zdjz5xxxrn6qqfc1816s6k >> /home/ethos/ethos_rig_mon.log
 
-@reboot curl -s -F "token=<app_token>" -F "user=<user_token> " -F "title=RIG - "$(hostname)" - IS NOW ONLINE" -F "message=RIG - "$(hostname)" rebooted and is now online." https://api.pushover.net/1/messages.json
-*/2 * * * *   /usr/bin/python /home/ethos/ethos_rig_mon.py -u <user_token> -a <app_token> >> /home/ethos/ethos_rig_mon.log
+@reboot curl -s -F "token=aardkds1zdjxxxx8rn6qqfc1816s6k" -F "user=uqvkvnrbxxxxtt4iap35bzs3n3jy1y " -F "title=RIG - "$(hostname)" - IS NOW ONLINE" -F "message=RIG - "$(hostname)" rebooted and is now online." https://api.pushover.net/1/messages.json
+*/2 * * * *   /usr/bin/python /home/ethos/ethos_rig_mon.py -u uqvkvnrbxxxxtt4iap35bzs3n3jy1y -a aardkds1zdjxxxx8rn6qqfc1816s6k >> /home/ethos/ethos_rig_mon.log
 
  """
 # get rig hostname
@@ -61,7 +60,7 @@ miner_id = socket.gethostname()
 
 # if the output file does not exist call the script with with log name to generate the output
 if not os.path.exists("/home/ethos/ethos_rig_mon.log"):
-    os.system("python ethos_rig_mon.py -u <user_token> -a <app_token> >> /home/ethos/ethos_rig_mon.log") 
+    os.system("python ethos_rig_mon.py -u uqvkvnrbxxxxtt4iap35bzs3n3jy1y -a aardkds1zdjxxxx8rn6qqfc1816s6k >> /home/ethos/ethos_rig_mon.log") 
     exit()
 
 # if the file has 362 lines (1 run every 2 minutes for 12 hours plus the header, if a previous output is found it is deleted, the output file is renamed to previous_ethos_rig_mon.log, and the script is executed again.
@@ -69,7 +68,7 @@ num_lines = sum(1 for line in open('/home/ethos/ethos_rig_mon.log'))
 if num_lines >= 362:
     os.remove("/home/ethos/previous_ethos_rig_mon.log") if os.path.exists("/home/ethos/previous_ethos_rig_mon.log") else None
     os.rename("/home/ethos/ethos_rig_mon.log","/home/ethos/previous_ethos_rig_mon.log")
-    os.system("python ethos_rig_mon.py -u <user_token> -a <app_token> >> /home/ethos/ethos_rig_mon.log") 
+    os.system("python ethos_rig_mon.py -u uqvkvnrbxxxxtt4iap35bzs3n3jy1y -a aardkds1zdjxxxx8rn6qqfc1816s6k >> /home/ethos/ethos_rig_mon.log") 
     exit()
 
 # write header in output file if the file is empty
@@ -118,7 +117,7 @@ with open(os.devnull, 'w') as DEVNULL:
     except subprocess.CalledProcessError:
         is_up = False
 
-# based on ping results, it either writes an offline log entry and exits, of if the result was online, the script continues.
+# based on ping results, it either writes an offline log entry and exits, of if the reslut was online, the script continues.
 if is_up <> True:
     print("{:%Y%m%d_%H%M}".format(current_time)),"|  {}".format(miner_id)," | OFFLINE |                | NO INTERNET CONNECTIVITY"
     exit()
@@ -130,7 +129,7 @@ varHashTot = 0
 varHashAvg = 0
 varCardHash = 0
 varCardLog = ""
-f = '/var/run/ethos/miner_hashes.file'
+f = '/var/run/ethos/miner_hashes.file' #miner hash file location
 # Get the last line from the file
 p = Popen(['tail','-1',f], stderr=PIPE, stdout=PIPE)
 res,err = p.communicate()
@@ -150,13 +149,15 @@ else:
             # check if file exists, meaning that the hash rate wasn't as expected during the last script run
             if os.path.exists("/tmp/ethos_rig_mon_gpu_"+varCardLog+".txt"):
                 os.remove("/tmp/ethos_rig_mon_gpu_"+varCardLog+".txt")
-                # send push notification if the tokens have been set using the pushover notification service
-                pushover_message(
-                    'RIG - {} - RESTARTING'.format(miner_id),
-                    'RIG - {} - GPU {} reached a hash rate of {}'.format(miner_id,varCardLog,hash),
-                    args.pushover_app_token,
-                    args.pushover_user_token
-                )
+                varNotifyCnt = varNotifyCnt + 1
+                if varNotifyCnt == 1:
+                    # send push notification
+                    pushover_message(
+                        'RIG - {} - RESTARTING'.format(miner_id),
+                        'RIG - {} - GPU {} reached a hash rate of {}'.format(miner_id,varCardLog,hash),
+                        args.pushover_app_token,
+                        args.pushover_user_token
+                    )
                 # reboot the system
                 print("{:%Y%m%d_%H%M}".format(current_time)),"|  {}".format(miner_id)," | ONLINE  |",spaces,"{}".format(hash),"MH/s  | REBOOTING RIG - GPU CHECK"
                 os.system("/opt/ethos/bin/r")
@@ -165,16 +166,16 @@ else:
                 # create a file so that next time we know we have been at 0 hash rate for one cycle.  If the problem is not cleared on the next cycle, the rig will be rebooted.
                 os.system('touch {}'.format("/tmp/ethos_rig_mon_gpu_"+varCardLog+".txt"))
         else:
-            # if the checkfile exists, remove it because the conditions are no longer met
-            #print "/tmp/ethos_rig_mon_gpu_"+varCardLog+".txt"
+            # if the check file exists, remove it because the low hash issue has cleared
             if os.path.exists("/tmp/ethos_rig_mon_gpu_"+varCardLog+".txt"):
                 os.remove("/tmp/ethos_rig_mon_gpu_"+varCardLog+".txt")
                 print("{:%Y%m%d_%H%M}".format(current_time)),"|  {}".format(miner_id)," | ONLINE  |",spaces,"{}".format(hash),"MH/s  | REMOVED CHECK FILE - GPU ",varCardNum
+# set more variables
 varHashAvg = (varHashTot/varCardNum)*varCardNum
 hashdec = format(float(varHashAvg), '.2f')
 spaces = x*(7-len(hashdec))
 
-# Check RIG average hash, if is non-existant of less than 115 by default - defined in the hashavgalert variable, a temp file is written, and if on the second run, the file still exists, the rig is rebooted.
+# Check RIG average hash, if is non-existent of less than 115 by default - defined in the hashavgalert variable, a temp file is written, and if on the second run, the file still exists, the rig is rebooted.
 if not float(hashdec) or float(hashdec) < float(hashavgalert):
     print("{:%Y%m%d_%H%M}".format(current_time)),"|  {}".format(miner_id)," | ONLINE  |",spaces,"{}".format(hashdec),"MH/s  |",errorstatusmessage
     # check if file exists, meaning that the hash rate wasn't as expected during the last script run
